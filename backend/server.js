0f95580abbea9ed76495ee9070359271b27fa69e
rakesh2604 <rakesh.kr2604@gmail.com>
2026-05-23 14:18:03 +0530

    Setup connection for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
